065ea432bcf5ae32f733ab4174409afa2d2b72d3
Christoph Schock <chschock@gmail.com>
2016-03-01 15:17:33 +0100

    abstract request calls and raise errors

--- src/latapi.py
+++ src/latapi.py
@@ -6,17 +6,162 @@ import collections
 
 CsvDef = collections.namedtuple('CsvDef', 'file textfield metafields')
 
-class LatApi():
-    def __init__(self, key, url="http://api.lateral.io"):
+
+class LatRequest():
+    """Basic requests to the Lateral API. Base class for higher level
+    API wrapper classes"""
+
+    def __init__(self, key, url="http://api.lateral.io", ignore=[406]):
         self.url_base = url
         self.key = key
+        self.ignore = ignore
 
-    def url(self, path_str):
-        return urljoin(self.url_base, path_str)
+    def _url(self, endpoint):
+        return urljoin(self.url_base, endpoint)
 
-    def hdr(self):
+    def _hdr(self):
         return {'content-type': 'application/json',
                 'subscription-key': self.key}
+
+    def _request(self, method, endpoint, params=None, data={}):
+        m = getattr(requests.api, method)
+        resp = m(self._url(endpoint), headers=self._hdr(), params=params, data=data)
+        if resp.status_code == 200 or self.ignore.count(resp.status_code):
+            return resp
+        print resp.json()
+        resp.raise_for_status()
+
+    def _get(self, endpoint, params={}):
+        return self._request('get', endpoint, params=params)
+
+    def _post(self, endpoint, data={}):
+        return self._request('post', endpoint, data=data)
+
+    def _put(self, endpoint, data):
+        return self._request('put', endpoint, data=data)
+
+    def _delete(self, endpoint, data={}):
+        return self._request('delete', endpoint, data=data)
+
+
+class LatApi(LatRequest):
+    """All Lateral API requests (but batch request"""
+
+    ###########
+    # Documents
+
+    def post_document(self, text, meta={}):
+        r = self._post('documents',
+            json.dumps({"text": text, "meta": json.dumps(meta)}))
+        return r
+
+    def put_document(self, id, text, meta={}):
+        r = self._post('documents/{}'.format(id),
+            json.dumps({"text": text, "meta": json.dumps(meta)}))
+        return r
+
+    def get_documents(self, keywords=None):
+        r = self._get('documents', {"keywords":keywords} if keywords else None)
+        return r
+
+    def get_document(self, id):
+        r = self._get('documents/{}'.format(id))
+        return r
+
+    def delete_document(self, id):
+        r = self._delete('documents/{}'.format(id))
+        return r
+
+    ###########
+    # Users
+
+    def post_user(self):
+        r = self._post('users')
+        return r
+
+    def get_users(self):
+        r = self._get('users')
+        return r
+
+    def get_user(self, id):
+        r = self._get('users/{}'.format(id))
+        return r
+
+    def delete_user(self, id):
+        r = self._delete('users/{}'.format(id))
+        return r
+
+    ###########
+    # Recommend
+
+    def get_user_recommendations(self, user_id, number, select_from=None):
+        r = self._get('users/{}/recommendations'.format(user_id),
+            params=json.dumps({"number": number, "select_from": select_from}))
+        return r
+
+    ###########
+    # Users
+
+    def post_preference(self, user_id, document_id):
+        r = self._post('users/{}/preferences/{}'.format(user_id, document_id))
+        return r
+
+    def get_preferences_collection(self, user_id):
+        r = self._get('users/{}/preferences'.format(user_id))
+        return r
+
+    def get_preferences(self, user_id, document_id):
+        r = self._get('users/{}/preferences/{}'.format(user_id, document_id))
+        return r
+
+    def delete_preferences(self, user_id, document_id):
+        r = self._delete('users/{}/preferences/{}'.format(user_id, document_id))
+        return r
+
+    ###########
+    # Clusters
+
+    def post_cluster_model(self, size):
+        r = self._post('cluster-models', data = '{"number_clusters":%d}'%(size))
+        return r
+
+    def get_cluster_models(self):
+        r = self._get('cluster-models')
+        return r
+
+    def get_cluster_model(self, id):
+        r = self._get('cluster-models/{}'.format(id))
+        return r
+
+    def delete_cluster_model(self, id):
+        r = self._delete('cluster-models/{}'.format(id))
+        return r
+
+    def get_clusters_collection(self, cluster_model_id):
+        r = self._get('cluster-models/{}/clusters'.format(cluster_model_id))
+        return r
+
+    def get_clusters_words_collection(self, cluster_model_id, cluster_id):
+        r = self._get('cluster-models/{}/clusters/{}/words'.format(
+            cluster_model_id, cluster_id))
+        return r
+
+    def get_clusters_word_cloud(self, cluster_model_id, cluster_id):
+        r = self._get('cluster-models/{}/clusters/{}/word-cloud'.format(
+                cluster_model_id, cluster_id))
+        return r
+
+    ###########
+    # Generic
+
+    def delete_all_data(self):
+        r = self._delete('delete-all-data')
+        return r
+
+
+class LatApiLoader(LatApi):
+    """Adds convenience functions to LatApi.
+    Batch requests load csv files and save wordcloud images."""
 
     def create_meta(self, row, metafields):
         meta = dict([(dst, row[src]) for src, dst in metafields.iteritems()])
@@ -76,157 +221,3 @@ class LatApi():
             r = self.get_clusters_word_cloud(cluster_model_id, c)
             with open( "{}{:02d}.png".format(file_prefix, c), 'wb') as f:
                 f.write(r.content)
-
-    ###########
-    # Documents
-
-    def post_document(self, text, meta={}):
-        r = requests.request('POST',
-            self.url('documents'),
-            headers = self.hdr(),
-            data = json.dumps({"text": text, "meta": meta}))
-        return r
-
-    def get_documents(self, keywords=None):
-        r = requests.request('GET',
-            self.url('documents'),
-            headers = self.hdr(),
-            params = {"keywords":keywords} if keywords else None)
-        return r
-
-    def get_document(self, id):
-        r = requests.request('GET',
-            self.url('documents/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    def delete_document(self, id):
-        r = requests.request('DELETE',
-            self.url('documents/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    ###########
-    # Users
-
-    def post_user(self):
-        r = requests.request('POST',
-            self.url('users'),
-            headers = self.hdr())
-        return r
-
-    def get_users(self):
-        r = requests.request('GET',
-            self.url('users'),
-            headers = self.hdr())
-        return r
-
-    def get_user(self, id):
-        r = requests.request('GET',
-            self.url('users/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    def delete_user(self, id):
-        r = requests.request('DELETE',
-            self.url('users/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    ###########
-    # Recommend
-
-    def get_user_recommendations(self, user_id, number, select_from=None):
-        r = requests.request('GET',
-            self.url('users/{}/recommendations'.format(user_id)),
-            headers = self.hdr(),
-            params = json.dumps({"number": number, "select_from": select_from}))
-        return r
-
-    ###########
-    # Users
-
-    def post_preference(self, user_id, document_id):
-        r = requests.request('POST',
-            self.url('users/{}/preferences/{}'.format(
-                user_id, document_id)),
-            headers = self.hdr())
-        return r
-
-    def get_preferences_collection(self, user_id):
-        r = requests.request('GET',
-            self.url('users/{}/preferences'.format(user_id)),
-            headers = self.hdr())
-        return r
-
-    def get_preferences(self, user_id, document_id):
-        r = requests.request('GET',
-            self.url('users/{}/preferences/{}'.format(
-                user_id, document_id)),
-            headers = self.hdr())
-        return r
-
-    def delete_preferences(self, user_id, document_id):
-        r = requests.request('DELETE',
-            self.url('users/{}/preferences/{}'.format(
-                user_id, document_id)),
-            headers = self.hdr())
-        return r
-
-    ###########
-    # Clusters
-
-    def post_cluster_model(self, size):
-        r = requests.request('POST',
-            self.url('cluster-models'),
-            headers = self.hdr(),
-            data = '{"number_clusters":%d}' % (size))
-        return r
-
-    def get_cluster_models(self):
-        r = requests.request('GET',
-            self.url('cluster-models'),
-            headers = self.hdr())
-        return r
-
-    def get_cluster_model(self, id):
-        r = requests.request('GET',
-            self.url('cluster-models/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    def delete_cluster_model(self, id):
-        r = requests.request('DELETE',
-            self.url('cluster-models/{}'.format(id)),
-            headers = self.hdr())
-        return r
-
-    def get_clusters_collection(self, cluster_model_id):
-        r = requests.request('GET',
-            self.url('cluster-models/{}/clusters'.format(cluster_model_id)),
-            headers = self.hdr())
-        return r
-
-    def get_clusters_words_collection(self, cluster_model_id, cluster_id):
-        r = requests.request('GET',
-            self.url('cluster-models/{}/clusters/{}/words'.format(
-                cluster_model_id, cluster_id)),
-            headers = self.hdr())
-        return r
-
-    def get_clusters_word_cloud(self, cluster_model_id, cluster_id):
-        r = requests.request('GET',
-            self.url('cluster-models/{}/clusters/{}/word-cloud'.format(
-                cluster_model_id, cluster_id)),
-            headers = self.hdr())
-        return r
-
-    ###########
-    # Generic
-
-    def delete_all_data(self):
-        r = requests.request('DELETE',
-            self.url('delete-all-data'),
-            headers = self.hdr())
-        return r
-
