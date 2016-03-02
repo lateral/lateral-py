2785ca47c63dda3893926ada4e1311094dfda35f
Christoph Schock <chschock@gmail.com>
2016-03-02 14:25:16 +0100

    seperate LatApi and csv LatApiLoader

---
+++ src/loader.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import requests, json, pandas as pd, collections
+import latapi
+
+CsvDef = collections.namedtuple('CsvDef', 'file textfield metafields')
+
+class LatApiLoader(latapi.LatApi):
+    """Adds convenience functions to LatApi.
+    Batch requests load csv files and save wordcloud images."""
+
+    def create_meta(self, row, metafields):
+        meta = dict([(dst, row[src]) for src, dst in metafields.iteritems()])
+        return meta
+
+    def create_batch_request_data(self, df, csvdef):
+        rows = [df.loc[ind] for ind in df.index]
+        opsdct = lambda row: {
+                'method': 'POST',
+                'url': '/documents',
+                'params': {'text': json.dumps(row[csvdef.textfield]),
+                           'meta': json.dumps(
+                                self.create_meta(row, csvdef.metafields))},
+                'headers': self.hdr()
+            }
+        ops = [opsdct(row) for row in rows]
+        return ops
+
+    def batch_post_request(self, ops):
+        data = json.dumps({'ops': ops, 'sequential': 'true'})
+        r = requests.request(
+            'POST',
+            self.url('batch'),
+            headers=self.hdr(),
+            data=data)
+        return r
+
+    def get_df(self, csvdef, chunksize):
+        return pd.read_csv(csvdef.file, chunksize=chunksize, na_filter=False)
+
+    def ingest(self, csvdef, batchsize=100, total=-1):
+        success_cnt = 0
+        for i, df in enumerate(self.get_df(csvdef, batchsize)):
+            ops = self.create_batch_request_data(df, csvdef)
+            r = self.batch_post_request(ops)
+            results = dict(r.json())["results"]
+            for j, res in enumerate(results):
+                if res["status"] != 201 and res["status"] != 406:
+                    print("Error for doc {doc} in batch {bat}:\n{res}".format(
+                        doc=j, bat=i, res=json.dumps(
+                            r.json(), sort_keys=True, indent=2)))
+                    print("Document was: \n{}".format(json.dumps(ops)))
+                else:
+                    success_cnt += 1
+
+            print("Chunk {}  status {}  {}".format(i, r.status_code, r.reason))
+            if success_cnt % 500 == 0:
+                gr = self.get_documents()
+                print("{} documents total".format(gr.headers['total']))
+            if total >= 0 and success_cnt >= total:
+                break
+
+    def save_wordclouds(self, cluster_model_id, file_prefix):
+        clusters = json.loads(
+            self.get_clusters_collection(cluster_model_id).content)
+        for c in clusters:
+            r = self.get_clusters_word_cloud(cluster_model_id, c)
+            with open( "{}{:02d}.png".format(file_prefix, c), 'wb') as f:
+                f.write(r.content)
