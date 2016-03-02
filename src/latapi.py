312157d4515742eae15cb8416948b158507eb589
Christoph Schock <chschock@gmail.com>
2016-03-02 13:56:00 +0100

    complete api and fix calls

--- src/latapi.py
+++ src/latapi.py
@@ -26,7 +26,7 @@ class LatRequest():
     def _request(self, method, endpoint, params=None, data={}):
         m = getattr(requests.api, method)
         resp = m(self._url(endpoint), headers=self._hdr(), params=params, data=data)
-        if resp.status_code == 200 or self.ignore.count(resp.status_code):
+        if resp.status_code / 100 == 2 or self.ignore.count(resp.status_code):
             return resp
         print resp.json()
         resp.raise_for_status()
@@ -37,7 +37,7 @@ class LatRequest():
     def _post(self, endpoint, data={}):
         return self._request('post', endpoint, data=data)
 
-    def _put(self, endpoint, data):
+    def _put(self, endpoint, data={}):
         return self._request('put', endpoint, data=data)
 
     def _delete(self, endpoint, data={}):
@@ -47,40 +47,40 @@ class LatRequest():
 class LatApi(LatRequest):
     """All Lateral API requests (but batch request"""
 
-    ###########
+    ######################
     # Documents
+
+    def get_documents(self, keywords=None):
+        r = self._get('documents', {"keywords":keywords} if keywords else None)
+        return r
 
     def post_document(self, text, meta={}):
         r = self._post('documents',
             json.dumps({"text": text, "meta": json.dumps(meta)}))
         return r
 
-    def put_document(self, id, text, meta={}):
-        r = self._post('documents/{}'.format(id),
-            json.dumps({"text": text, "meta": json.dumps(meta)}))
-        return r
-
-    def get_documents(self, keywords=None):
-        r = self._get('documents', {"keywords":keywords} if keywords else None)
-        return r
-
     def get_document(self, id):
         r = self._get('documents/{}'.format(id))
+        return r
+
+    def put_document(self, id, text, meta={}):
+        r = self._put('documents/{}'.format(id),
+            json.dumps({"text": text, "meta": json.dumps(meta)}))
         return r
 
     def delete_document(self, id):
         r = self._delete('documents/{}'.format(id))
         return r
 
-    ###########
+    ######################
     # Users
-
-    def post_user(self):
-        r = self._post('users')
-        return r
 
     def get_users(self):
         r = self._get('users')
+        return r
+
+    def post_user(self):
+        r = self._post('users')
         return r
 
     def get_user(self, id):
@@ -91,42 +91,39 @@ class LatApi(LatRequest):
         r = self._delete('users/{}'.format(id))
         return r
 
-    ###########
-    # Recommend
-
     def get_user_recommendations(self, user_id, number, select_from=None):
         r = self._get('users/{}/recommendations'.format(user_id),
             params=json.dumps({"number": number, "select_from": select_from}))
         return r
 
-    ###########
-    # Users
+    ######################
+    # Preferences
+
+    def get_preferences(self, user_id):
+        r = self._get('users/{}/preferences'.format(user_id))
+        return r
+
+    def get_preference(self, user_id, document_id):
+        r = self._get('users/{}/preferences/{}'.format(user_id, document_id))
+        return r
 
     def post_preference(self, user_id, document_id):
         r = self._post('users/{}/preferences/{}'.format(user_id, document_id))
         return r
 
-    def get_preferences_collection(self, user_id):
-        r = self._get('users/{}/preferences'.format(user_id))
-        return r
-
-    def get_preferences(self, user_id, document_id):
-        r = self._get('users/{}/preferences/{}'.format(user_id, document_id))
-        return r
-
-    def delete_preferences(self, user_id, document_id):
+    def delete_preference(self, user_id, document_id):
         r = self._delete('users/{}/preferences/{}'.format(user_id, document_id))
         return r
 
-    ###########
+    ######################
     # Clusters
-
-    def post_cluster_model(self, size):
-        r = self._post('cluster-models', data = '{"number_clusters":%d}'%(size))
-        return r
 
     def get_cluster_models(self):
         r = self._get('cluster-models')
+        return r
+
+    def post_cluster_model(self, size):
+        r = self._post('cluster-models', data = '{"number_clusters":%d}'%(size))
         return r
 
     def get_cluster_model(self, id):
@@ -137,11 +134,16 @@ class LatApi(LatRequest):
         r = self._delete('cluster-models/{}'.format(id))
         return r
 
-    def get_clusters_collection(self, cluster_model_id):
+    def get_clusters(self, cluster_model_id):
         r = self._get('cluster-models/{}/clusters'.format(cluster_model_id))
         return r
 
-    def get_clusters_words_collection(self, cluster_model_id, cluster_id):
+    def get_clusters_documents(self, cluster_model_id, cluster_id):
+        r = self._get('cluster-models/{}/clusters/{}/documents'.format(
+            cluster_model_id, cluster_id))
+        return r
+
+    def get_clusters_words(self, cluster_model_id, cluster_id):
         r = self._get('cluster-models/{}/clusters/{}/words'.format(
             cluster_model_id, cluster_id))
         return r
@@ -151,7 +153,7 @@ class LatApi(LatRequest):
                 cluster_model_id, cluster_id))
         return r
 
-    ###########
+    ######################
     # Generic
 
     def delete_all_data(self):
